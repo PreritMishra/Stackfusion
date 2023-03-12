834d46b651916fd16c87f7b2ee9f4d61b4fd8c5d
Prerit <preritmishra1002@gmail.com>
2023-03-13 01:22:35 +0530

    Performed validation

--- src/frontend/form.js
+++ src/frontend/form.js
@@ -30,27 +30,53 @@ import "./form.css";
 function Form() {
   const [name, setName] = useState("");
   const [dob, setDob] = useState("");
+  const [age, setAge] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [isValidDob, setIsValidDob] = useState(true);
 
   const handleNameChange = (event) => {
+    console.log(event.target.value);
     setName(event.target.value);
   };
 
   const handleDobChange = (event) => {
+    console.log(event.target.value);
     setDob(event.target.value);
   };
 
   const handleEmailChange = (event) => {
+    console.log(event.target.value);
     setEmail(event.target.value);
   };
 
   const handlePhoneChange = (event) => {
+    console.log(event.target.value);
     setPhone(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const dobDate = new Date(dob);
+        const ageInMs = Date.now() - dobDate.getTime();
+        const ageDate = new Date(ageInMs);
+        const aage = Math.abs(ageDate.getUTCFullYear() - 1970); // Calculate the age in years
+        //setAge(aage);
+        console.log("This is age" , aage);
+        if(aage<18){
+          alert('User is less than 18');
+          return;
+        }
+        const re = /\S+@\S+\.\S+/;
+        if (!re.test(email)) {
+            alert('Please enter a valid email address');
+            return;
+        }
+        const phoneRegex = /^\d{10}$/; // regular expression to match 10-digit phone number format
+        if (!phoneRegex.test(phone)) {
+            alert('Phone number must be in 10-digit format');
+            return;
+        }
     console.log({ name, dob, email, phone });
   };
 
@@ -59,22 +85,39 @@ function Form() {
     <form onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" value={name} onChange={handleNameChange} />
+        <input type="text" value={name}
+          onChange={
+            handleNameChange
+          }
+        />
       </label>
       <br />
       <label>
         Date of Birth:
-        <input type="date" value={dob} onChange={handleDobChange} />
-      </label>
+        <input type="date" value={dob}
+        max={new Date().toISOString().split("T")[0]}
+         onChange={
+          handleDobChange
+          } />
+        </label>
       <br />
       <label>
         Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
+        <input type="email" value={email}
+
+         onChange={
+          handleEmailChange
+          } 
+        />
       </label>
       <br />
       <label>
         Phone Number:
-        <input type="tel" value={phone} onChange={handlePhoneChange} />
+        <input type="tel" value={phone}
+         onChange={
+          handlePhoneChange
+          }
+        />
       </label>
       <br />
       <button type="submit">Submit</button>
